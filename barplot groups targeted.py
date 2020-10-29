0bb0b97f6bec1f19126f32b83ff6e6a7e2d79de2
annahiller <annavhiller@gmail.com>
2020-10-29 14:17:55 +0100

    Update barplot groups targeted.py

--- barplot groups targeted.py
+++ barplot groups targeted.py
@@ -11,11 +11,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #window = window_list[0]  import window dataset instead of this
+window['targeted_group1'].replace({'USDP': 'Party supporters', 'Rohingya/EAOs': 'Ethnic/racial groups', 'Ethical/racial groups':'Ethnic/racial groups','Tatmadaw': 'Military', 'Kachin': 'Ethnic/racial groups', 'Journalist':'Journalist/activist', 'Journalist/activists':'Journalist/activist', 'Muslim':'Ethnic/racial groups', 'NLD':'Party supporters', 'Activist':'Journalist/activist', 'Bamar':'Ethnic/racial groups', 'Migrant workers':'General', 'Tatmadaw/Government': 'Military', 'Chinese':'Ethnic/racial groups', 'Ethnic minority/Migrant workers':'Ethnic/racial groups', 'Commander in Chief':'Military','Prisoner':'General', 'Government':'Military', 'KIA':'Ethnic/racial groups', 'Ethnic minority':'Ethnic/racial groups', 'Rohingya':'Ethnic/racial groups', 'Pro Tatmadaw':'Party supporters', 'AA':'Ethnic/racial groups', 'Rakhine':'Ethnic/racial groups', 'Activist/Journalist':'Journalist/activist', 'Lisu':'Ethnic/racial groups', 'Lawwal':'Ethnic/racial groups', 'Pro Tatmadaw ':'Party supporters', 'Ethnical/racial groups':'Ethnic/racial groups'}, inplace=True)
 target_count = window['targeted_group1'].value_counts()
 
 target_count = target_count.reset_index()
 target_count.rename(columns={'index':'group', 'targeted_group1':'count'}, inplace=True)
-target_count = target_count[:10]
 
 count = target_count['count']
 total_count = count.sum()
